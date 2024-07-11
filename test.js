01e6708f4db12285c14db2c5e0daec55e5373d59
s.malovitsa <stas.malovitsa@gmail.com>
2024-07-11 22:26:09 +0300

    initialized

---
+++ test.js
@@ -0,0 +1,59 @@
+'use strict'
+
+// const aB= {
+//     a: 3+2,
+//     b: 2+2    
+// }
+// console.log(aB)
+
+// A = 123
+
+// function A() {
+//     console.log('A')
+// }  
+
+// const myCity = {
+//     city: "Kyiv",
+//     cityGreeting: function () {
+//         console.log("Dobryi vechir!")
+//     }
+// }
+
+// myCity.cityGreeting()
+
+
+// function myFn() {
+//     let b 
+//     a = a + 1
+//     c = a + b
+//     return c
+// }
+
+// myFn(2, 3) 
+
+// console.log(myFn)
+
+// let a 
+// let b 
+
+// function myFn(){
+//     let b 
+//     a = true 
+//     b = 10
+//     console.log(b) 
+// }
+
+// myFn()
+// console.log(a)
+// console.log(b)
+
+// const a = 5
+
+function myFn(){
+    function innerFn(){
+        console.log(a)
+    }
+    innerFn()
+}
+
+myFn()
