e4f9812cfbb0a6a52041fe61abb739c9fd848196
s.malovitsa <stas.malovitsa@gmail.com>
2024-07-20 15:24:08 +0300

    added async/rpomise func

--- test.js
+++ test.js
@@ -236,3 +236,16 @@
 //     h++
 // } while (h<5)
 
+const timerPromise = () =>
+    new Promise((resolve, reject) =>
+    setTimeout(() => resolve(), 2000))
+
+const asyncFn = async () => {
+    console.log('Timer starts')
+    const startTime = performance.now()
+    await timerPromise()
+    const endTime = performance.now()
+    console.log('Timer ended', endTime - startTime)
+}
+
+asyncFn()
